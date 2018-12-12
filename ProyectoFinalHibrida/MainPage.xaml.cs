0e447c5764145da80099eaa29d63e358c29e472b
AlejandroGorgues <alegorval@gmail.com>
2018-12-12 14:24:24 +0100

    Added basics elements to XAML

--- ProyectoFinalHibrida/MainPage.xaml.cs
+++ ProyectoFinalHibrida/MainPage.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -22,9 +24,26 @@ namespace ProyectoFinalHibrida
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Geolocator GPS = new Geolocator();
+        MapIcon mapIcon = new MapIcon();
+
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Geoposition pos = await GPS.GetGeopositionAsync();
+            Windows.UI.Core.DispatchedHandler Lectura_Posicion = () =>
+            {
+                mapView.Center = pos.Coordinate.Point;
+                mapView.ZoomLevel = 15;
+                mapIcon.Location = pos.Coordinate.Point;
+                mapView.MapElements.Add(mapIcon);
+            };
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, Lectura_Posicion);
         }
     }
 }
